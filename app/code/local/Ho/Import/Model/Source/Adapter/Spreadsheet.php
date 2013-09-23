d27ef9f5e1d18805ed8154486eba740e45d5fdae
Paul Hachmang <paul@h-o.nl>
2013-09-23 11:42:32 +0200

    [+FEAT] Spreadsheet reader (XLS, XLSX, CSV, ODF)

---
+++ app/code/local/Ho/Import/Model/Source/Adapter/Spreadsheet.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Ho_Import
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_Import
+ * @copyright   Copyright © 2013 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Paul Hachmang – H&O <info@h-o.nl>
+ *
+ * XML streamer based on https://github.com/prewk/XmlStreamer/blob/master/XmlStreamer.php
+ */
+include_once 'spreadsheet-reader/php-excel-reader/excel_reader2.php';
+include_once 'spreadsheet-reader/SpreadsheetReader.php';
+
+class Ho_Import_Model_Source_Adapter_Spreadsheet extends SpreadsheetReader
+{
+
+    protected $_hasHeaders = false;
+    /**
+     * If the first line of the document contains headers, it will be stored here and each row will
+     * return a key-value.
+     *
+     * @var array
+     */
+    protected $_colNames = array();
+
+    public function __construct(array $config) {
+        $source = is_string($config) ? $config : $config['file'];
+        if (!is_string($source)) {
+            Mage::throwException(Mage::helper('importexport')->__('Source file path must be a string'));
+        }
+        if (!is_readable($source)) {
+            Mage::throwException(Mage::helper('importexport')->__("%s file does not exists or is not readable", $source));
+        }
+        if (!is_file($source)) {
+            Mage::throwException(Mage::helper('importexport')->__("%s isn't a file, probably a folder.", $source));
+        }
+        $this->_source = $source;
+        parent::__construct($this->_source);
+
+        if ($config['has_headers'] && is_numeric($config['has_headers'])) {
+            $this->_hasHeaders = true;
+            $this->rewind();
+        }
+
+        return $this;
+    }
+
+    public function rewind() {
+        parent::rewind();
+
+        if ($this->_hasHeaders) {
+            $current = $this->current();
+            $this->_colNames = $current;
+        }
+    }
+
+    public function current() {
+        if ($this->_hasHeaders && count($this->_colNames)) {
+            $row = parent::current();
+            foreach ($this->_colNames as $index => $key) {
+                $row[$key] = $row[$index];
+                unset($row[$index]);
+            }
+
+            return $row;
+        } else {
+            return parent::current();
+        }
+    }
+}
