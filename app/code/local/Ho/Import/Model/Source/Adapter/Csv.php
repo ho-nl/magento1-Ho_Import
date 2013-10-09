7bdc6491e9a5e6d6ca8caa36a769de24a4909acd
Paul Hachmang <paul@h-o.nl>
2013-10-10 00:08:36 +0200

    [+FEAT] Added more configurable CSV reader (set delimiter en encapsulation)

---
+++ app/code/local/Ho/Import/Model/Source/Adapter/Csv.php
@@ -0,0 +1,260 @@
+<?php
+/**
+ * Ho_Import
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the H&O Commercial License
+ * that is bundled with this package in the file LICENSE_HO.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.h-o.nl/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@h-o.com so we can send you a copy immediately.
+ *
+ * @category    Ho
+ * @package     Ho_Import
+ * @copyright   Copyright © 2013 H&O (http://www.h-o.nl/)
+ * @license     H&O Commercial License (http://www.h-o.nl/license)
+ * @author      Paul Hachmang – H&O <info@h-o.nl>
+ *
+ * XML streamer based on https://github.com/prewk/XmlStreamer/blob/master/XmlStreamer.php
+ */
+class Ho_Import_Model_Source_Adapter_Csv implements SeekableIterator
+{
+
+    /**
+     * Column names array.
+     *
+     * @var array
+     */
+    protected $_colNames;
+
+    /**
+     * Quantity of columns in first (column names) row.
+     *
+     * @var int
+     */
+    protected $_colQuantity;
+
+    /**
+     * Current row.
+     *
+     * @var array
+     */
+    protected $_currentRow = null;
+
+    /**
+     * Current row number.
+     *
+     * @var int
+     */
+    protected $_currentKey = null;
+
+    /**
+     * Source file path.
+     *
+     * @var string
+     */
+    protected $_source;
+
+    /**
+     * Field delimiter.
+     *
+     * @var string
+     */
+    protected $_delimiter = ',';
+
+    /**
+     * Field enclosure character.
+     *
+     * @var string
+     */
+    protected $_enclosure = '"';
+
+    /**
+     * Source file handler.
+     *
+     * @var resource
+     */
+    protected $_fileHandler;
+
+
+    /**
+     * Adapter object constructor.
+     *
+     * @param array $config
+     * @return \Ho_Import_Model_Source_Adapter_Csv
+     */
+    final public function __construct(array $config)
+    {
+        if (!isset($config['file']) && !is_string($config['file'])) {
+            Mage::throwException(Mage::helper('importexport')->__('File path must be a string'));
+        }
+        if (!is_readable($config['file'])) {
+            Mage::throwException(Mage::helper('importexport')->__("%s file does not exists or is not readable", $config['file']));
+        }
+        $this->_source = $config['file'];
+
+        if (isset($config['delimiter'])) {
+            $this->_delimiter = (string) $config['delimiter'];
+        }
+
+        if (isset($config['enclosure'])) {
+            if ($config['enclosure']) {
+                $this->_enclosure = (string) $config['enclosure'];
+            } else {
+                $this->_enclosure = chr(0);
+            }
+
+        }
+
+        $this->_init();
+
+        // validate column names consistency
+        if (is_array($this->_colNames) && !empty($this->_colNames)) {
+            $this->_colQuantity = count($this->_colNames);
+
+            if (count(array_unique($this->_colNames)) != $this->_colQuantity) {
+                Mage::throwException(Mage::helper('importexport')->__('Column names have duplicates'));
+            }
+        } else {
+            Mage::throwException(Mage::helper('importexport')->__('Column names is empty or is not an array'));
+        }
+    }
+
+    /**
+     * Object destructor.
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        if (is_resource($this->_fileHandler)) {
+            fclose($this->_fileHandler);
+        }
+    }
+
+    /**
+     * Method called as last step of object instance creation. Can be overrided in child classes.
+     *
+     * @return Ho_Import_Model_Source_Adapter_Csv
+     */
+    protected function _init()
+    {
+        $this->_fileHandler = fopen($this->_source, 'r');
+        $this->rewind();
+        return $this;
+    }
+
+    /**
+     * Move forward to next element
+     *
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        $this->_currentRow = fgetcsv($this->_fileHandler, null, $this->_delimiter, $this->_enclosure);
+        $this->_currentKey = $this->_currentRow ? $this->_currentKey + 1 : null;
+    }
+
+    /**
+     * Rewind the Iterator to the first element.
+     *
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        // rewind resource, reset column names, read first row as current
+        rewind($this->_fileHandler);
+
+        $this->_colNames = fgetcsv($this->_fileHandler, null, $this->_delimiter, $this->_enclosure);
+        $this->_currentRow = fgetcsv($this->_fileHandler, null, $this->_delimiter, $this->_enclosure);
+
+        if ($this->_currentRow) {
+            $this->_currentKey = 0;
+        }
+    }
+
+    /**
+     * Seeks to a position.
+     *
+     * @param int $position The position to seek to.
+     * @throws OutOfBoundsException
+     * @return void
+     */
+    public function seek($position)
+    {
+        if ($position != $this->_currentKey) {
+            if (0 == $position) {
+                return $this->rewind();
+            } elseif ($position > 0) {
+                if ($position < $this->_currentKey) {
+                    $this->rewind();
+                }
+                while ($this->_currentRow = fgetcsv($this->_fileHandler, null, $this->_delimiter, $this->_enclosure)) {
+                    if (++ $this->_currentKey == $position) {
+                        return;
+                    }
+                }
+            }
+            throw new OutOfBoundsException(Mage::helper('importexport')->__('Invalid seek position'));
+        }
+    }
+
+
+    /**
+     * Return the current element.
+     *
+     * @return mixed
+     */
+    public function current()
+    {
+        return array_combine(
+            $this->_colNames,
+            count($this->_currentRow) != $this->_colQuantity
+                    ? array_pad($this->_currentRow, $this->_colQuantity, '')
+                    : $this->_currentRow
+        );
+    }
+
+    /**
+     * Column names getter.
+     *
+     * @return array
+     */
+    public function getColNames()
+    {
+        return $this->_colNames;
+    }
+
+    /**
+     * Return the key of the current element.
+     *
+     * @return int More than 0 integer on success, integer 0 on failure.
+     */
+    public function key()
+    {
+        return $this->_currentKey;
+    }
+
+    /**
+     * Checks if current position is valid.
+     *
+     * @return boolean Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        return !empty($this->_currentRow);
+    }
+
+    /**
+     * Check source file for validity.
+     *
+     * @return Ho_Import_Model_Source_Adapter_Csv
+     */
+    public function validateSource()
+    {
+        return $this;
+    }
+}
