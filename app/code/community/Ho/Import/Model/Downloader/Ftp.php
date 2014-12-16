cd15d1a2bc222aa137f0cf4a465745dfe2463ce8
Paul Hachmang <paul@h-o.nl>
2014-12-16 21:03:28 +0100

    Better warning if folder is not writable.

--- app/code/community/Ho/Import/Model/Downloader/Ftp.php
+++ app/code/community/Ho/Import/Model/Downloader/Ftp.php
@@ -26,27 +26,32 @@ class Ho_Import_Model_Downloader_Ftp extends Ho_Import_Model_Downloader_Abstract
 
     public function download(Varien_Object $connectionInfo, $target)
     {
-
         $file = $connectionInfo->getFile();
 
         $this->_log($this->_getLog()->__("Connecting to FTP server %s", $connectionInfo->getHost()));
 
         $ftp = new Varien_Io_Ftp();
         $ftp->open(array(
-            'host'     => $connectionInfo->getHost(),
-            'user'     => $connectionInfo->getUsername(),
-            'password' => $connectionInfo->getPassword(),
-            'timeout'  => $connectionInfo->hasTimeout() ? $connectionInfo->getTimeout() : 10,
-            'passive'  => $connectionInfo->hasPassive() ? $connectionInfo->getPassive() : true,
-            'ssl'      => $connectionInfo->hasSsl() ? $connectionInfo->getSsl() : null,
-            'file_mode'      => $connectionInfo->hasFileMode() ? $connectionInfo->getFileMode() : null,
+            'host'      => $connectionInfo->getHost(),
+            'user'      => $connectionInfo->getUsername(),
+            'password'  => $connectionInfo->getPassword(),
+            'timeout'   => $connectionInfo->hasTimeout() ? $connectionInfo->getTimeout() : 10,
+            'passive'   => $connectionInfo->hasPassive() ? $connectionInfo->getPassive() : true,
+            'ssl'       => $connectionInfo->hasSsl() ? $connectionInfo->getSsl() : null,
+            'file_mode' => $connectionInfo->hasFileMode() ? $connectionInfo->getFileMode() : null,
         ));
+
+        if (!is_writable(Mage::getBaseDir().DS.$target)) {
+            Mage::throwException($this->_getLog()->__(
+                "Can not write file %s to %s, folder not writable (doesn't exist?)",
+                $connectionInfo->getFile(), $target
+            ));
+        }
 
         $this->_log($this->_getLog()->__(
             "Downloading file %s from %s, to %s",
             $connectionInfo->getFile(), $connectionInfo->getHost(), $target
         ));
-
         $targetPath = $this->_getTargetPath($target, basename($file));
         $ftp->read($file, $targetPath);
         $ftp->close();
