96ce9270dfb99445f17d5a0f55002f503ad11420
Chris Jasperse <chris@chrisjasperse.local>
2016-03-18 11:16:59 +0100

    DIO-352: Fixed bug where getImportData returned NULL in source model

--- app/code/community/Ho/Import/Model/Import.php
+++ app/code/community/Ho/Import/Model/Import.php
@@ -825,7 +825,8 @@ class Ho_Import_Model_Import extends Varien_Object
 
         $this->_runEvent('source_adapter_before', $this->getProfile());
         $this->_getLog()->log($this->_getLog()->__('Getting source adapter %s', $source->getAttribute('model')));
-        $arguments = array_merge($arguments, $this->getImportData());
+        $importData = $this->getImportData();
+        $arguments = array_merge($arguments, is_null($importData) ? [] : $importData);
         $this->_sourceAdapter = Mage::getModel($source->getAttribute('model'), $arguments);
 
         if (!$this->_sourceAdapter) {
